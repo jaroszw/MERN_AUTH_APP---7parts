7712793a8458eb09261afafcf85a98a8c5b9f43c
Wojciech Jarosz <jaroszw@gmail.com>
2021-10-23 19:35:34 +0200

    Login, SignUp, Dash Components with Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dash from './pages/Dash';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 import './App.css';
-import { makeStyles } from '@mui/styles';
-import { Button } from '@mui/material';
-
-const useStyles = makeStyles({
-  butt: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-  },
-});
 
 function App() {
-  const classes = useStyles();
-
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-
-      <Button variant="contained" className={classes.butt}>
-        button
-      </Button>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/dash" component={Dash} />
+        </Switch>
+      </Router>
     </div>
   );
 }
